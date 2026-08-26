d3d0797236e3011b81e6ff04e33693cf72d72ff2
Lay10000 <126015391+Lay10000@users.noreply.github.com>
2026-08-26 21:29:25 +0200

    Implemented the first part of the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/navbar";
+import Hero from "./components/hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 
 const App = () => {
     return(
-        <div class="flex-center h-[100vh]">
-            <h1 className="text-3xl text-indigo-300">HI, G</h1>
-        </div>
+        <main>
+            <Navbar />
+            <Hero />
+            <div className="h-dvh bg-black">
+
+            </div>
+        </main>
     )
 }
 export default App
