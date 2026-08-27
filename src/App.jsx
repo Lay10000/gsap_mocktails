e49067abb315352f7272e8c0d414ba977d64d023
Lay10000 <126015391+Lay10000@users.noreply.github.com>
2026-08-27 20:18:33 +0200

    Scaling Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/navbar";
@@ -11,9 +11,28 @@ import Contact from "./components/contact";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-
 const App = () => {
-    return(
+//FIX FOR DIFFERNT WINDOWS SCALE RESOLUTIONS:
+    useEffect(() => {
+        const scale = window.devicePixelRatio; 
+        
+        const standardBase = 16; 
+        
+        if (scale > 1) {
+            const targetFontSize = standardBase / scale;
+            document.documentElement.style.fontSize = `${targetFontSize}px`;
+        } else {
+            document.documentElement.style.fontSize = "16px";
+        }
+        const timer = setTimeout(() => {
+            ScrollTrigger.refresh();
+        }, 150);
+
+        return () => clearTimeout(timer);
+    }, []);
+// --------------------------------------------
+
+    return (
         <main>
             <Navbar />
             <Hero />
@@ -23,6 +42,7 @@ const App = () => {
             <Menu />
             <Contact />
         </main>
-    )
-}
-export default App
+    );
+};
+
+export default App;
