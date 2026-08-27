c6bab6f77755c255ce15277336c8caa41d923821
Lay10000 <126015391+Lay10000@users.noreply.github.com>
2026-08-27 23:25:17 +0200

    Noise fix 5

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -3,8 +3,7 @@ import { SplitText } from "gsap/all"
 import gsap from "gsap"
 import { useRef } from "react"
 import { useMediaQuery } from "react-responsive"
-import noiseImg from '/src/images/noise.png';
-
+import noiseImg from '/images/noise.png';
 const Hero = () => {
     const videoRef = useRef();
 
